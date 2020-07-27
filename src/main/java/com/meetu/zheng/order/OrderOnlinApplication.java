b7f71fa1bb43dbcc8661331279b8c549aaa6b6ed
Tfenzheng <839811269@qq.com>
2020-07-27 21:16:21 +0800

    project init

---
+++ src/main/java/com/meetu/zheng/order/OrderOnlinApplication.java
@@ -0,0 +1,13 @@
+package com.meetu.zheng.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderOnlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderOnlinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetu/zheng/order/OrderOnlinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetu.zheng.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderOnlinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
